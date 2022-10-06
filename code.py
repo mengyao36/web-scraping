7b97242b9e78e61e46894b8db21c5a847d966b15
mengyao36 <mengyao.hu@stonybrook.edu>
2022-10-06 15:31:48 -0400

    update code and data

--- code.py
+++ code.py
@@ -1,109 +1,94 @@
+# import needed packages
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
-page = requests.get('https://github.com/trending')
+#####################################################################
+# first webpage
+page1 = requests.get('https://www.alz.org/news/2022')
 
 # Create a BeautifulSoup object
-soup = BeautifulSoup(page.text, 'html.parser')
+soup1 = BeautifulSoup(page1.text, 'html.parser')
 
 # print pretty
-print(soup.prettify())
+print(soup1.prettify())
 
-
-# get the text from each repo
-descriptions = soup.find_all('p',class_='col-9 color-fg-muted my-1 pr-4')
-output_descriptions = []
-for i in descriptions: #for x in y: 
-    print(i.text)
-    data = i.text
-    output_descriptions.append(data)
-
-len(output_descriptions)
-output_descriptions[1]
-output_descriptions[3]
-
-
-repo_stars = soup.find_all('a', class_='Link--muted d-inline-block mr-3')
-for i in repo_stars:
-    print('Count: ', i.text)
-
-# get the programming language from each repo
-p_langauge = soup.find_all('span',attrs={'itemprop': 'programmingLanguage'})
-# for each item in p_langauge, print the text
-for language in p_langauge:
-    print(language.text)
-
-
-# find each article where class='Box-row'
-articles = soup.find_all('article', class_='Box-row')
-# get length of articles
-len(articles)
-
-
-
-# get the div that contains data-hpc
-articles = soup.find_all(attrs={"data-hpc":True})  ## way one 
-articles = soup.find_all('div',attrs={'data-hpc':True}) ## way two 
-
-
-### by looking at the website, can see this is where the info is stored: 
-# <h1 class=h3 lh-condensed  ---- this is for the name of the repo 
-# <p1 class=col-9 color-fg-muted my-1 pr-4 ---- this is for the description of the repo
-
-# get the name of the repo and print it
-repo_name = soup.find_all('h1',class_='h3 lh-condensed')
-
-repo_names = []
-
-for item in repo_name:
-    name = item.text 
-    # print('Part 1: Raw: ', name)
-    ## clean name remove whitespace
-    name = name.strip()   
-    # print('Part 2: Updated: ', name)
-    ## remove new line
+# get names of each article listed in "Recent News"
+head_name = soup1.find_all('h3',class_='card-title')
+head_names = []
+for n in head_name:
+    name = n.text
+    name = name.strip()
     name = name.replace('\n','')
-    # print('Part 3: Updated: ', name)
-    ## remove all white space
-    name = name.replace(' ','')
-    # print('Part 4: Updated: ', name)
-    repo_names.append(name)
+    head_names.append(name)
+len(head_names)
 
-len(repo_names)
+# get published date of each article listed in "Recent News"
+foot_name =soup1.find_all('div',class_='card-footer')
+foot_names = []
+for f in foot_name:
+    foot = f.text
+    foot = foot.strip()
+    foot = foot.replace('\n','')
+    foot_names.append(foot)
+len(foot_names)
 
-for names in repo_names: 
-    print(names)
+# put these two pieces of information together into a dataframe
+df1 = pd.DataFrame({'title':head_names,'date':foot_names})
 
+df1.to_csv('data/web1.csv')
 
+#####################################################################
+# second webpage
+page2 = requests.get('https://weather.com/weather/tenday/l/Long+Island+City+NY+USNY0833:1:US')
 
+# Create a BeautifulSoup object
+soup2 = BeautifulSoup(page2.text, 'html.parser')
 
-# get the description of the repo and print it
-repo_desc = soup.find_all('p',class_='col-9 color-fg-muted my-1 pr-4')
+# print pretty
+print(soup2.prettify())
 
-repo_descs = []
-for item in repo_desc:
-    print(item.text)
-    desc = item.text
-    print('Part 1: Raw: ', desc)
-    ## clean name remove whitespace
-    desc = desc.strip()
-    print('Part 2: Updated: ', desc)
-    ## remove new line
-    desc = desc.replace('\n','')
-    print('Part 3: Updated: ', desc)
-    repo_descs.append(desc)
-len(repo_descs)
+# get date for each day listed 
+date = soup2.find_all('h3',class_='DetailsSummary--daypartName--2FBp2')
+dates = []
+for d in date:
+    day = d.text
+    day = day.strip()
+    day = day.replace('\n','')
+    dates.append(day)
+len(dates)
 
+# get lowest temp value for each day listed 
+low_temp = soup2.find_all('span',attrs={'data-testid': 'lowTempValue'})
+low_temps = []
+for l in low_temp:
+    templ = l.text
+    templ = templ.strip()
+    templ = templ.replace('\n','')
+    low_temps.append(templ)
+len(low_temps)
 
-list1 = repo_names 
-list2 = repo_descs
+# get highest temp value for each day listed 
+high_temp = soup2.find_all('span',attrs={'data-testid': 'TemperatureValue'},class_='DetailsSummary--highTempValue--3Oteu')
+high_temps = []
+for h in high_temp:
+    temph = h.text
+    temph = temph.strip()
+    temph = temph.replace('\n','')
+    high_temps.append(temph)
+len(high_temps)
 
-# create dictionary
-dictionary = {'names': list1, 'description': list2}
+# get description for each day listed 
+descrb = soup2.find_all('span',class_='DetailsSummary--extendedData--365A_')
+descrbs = []
+for de in descrb:
+    description = de.text
+    description = description.strip()
+    description = description.replace('\n','')
+    descrbs.append(description)
+len(descrbs)
 
-## put this together into a dataframe
-df = pd.DataFrame({'names':repo_names,'descriptions':repo_descs})
+# put these pieces of information together into a dataframe
+df2 = pd.DataFrame({'Date':dates,'Low_temp':low_temps,'High_temp':high_temps,'Description':descrbs})
 
-df.to_csv('/Users/hantswilliams/Desktop/githubtrending.csv')
+df2.to_csv('data/web2.csv')
